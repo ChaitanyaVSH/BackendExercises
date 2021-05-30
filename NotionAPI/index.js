aa56510095f44dccd6b045093b3a500194c26360
Chaitanya <kotichai@amazon.com>
2021-05-30 19:44:48 +0530

    Implemented notion client, uses notion client to list databases

--- NotionAPI/index.js
+++ NotionAPI/index.js
@@ -1,1 +1,18 @@
-console.log("This is running")
+const dotenv = require("dotenv").config();
+
+const { Client } = require('@notionhq/client');
+
+// Params that the client can accept
+const clientParams = {
+    auth: process.env.NOTION_ACCESS_TOKEN
+}
+// Initiate notion client
+const notion = new Client(clientParams);
+
+
+const listDB = async () => {
+    const result = await notion.databases.list();
+    console.log("DBs are: ", result);
+}
+
+listDB();
