5ba3a71588a6f7b2a80b65560bd697ba80948a67
Chaitanya <kotichai@amazon.com>
2021-05-30 20:06:43 +0530

    Querying a database and pulling the entries

--- NotionAPI/index.js
+++ NotionAPI/index.js
@@ -9,10 +9,28 @@ const clientParams = {
 // Initiate notion client
 const notion = new Client(clientParams);
 
+// Listing all the databases linked to the integrations
+// const listDB = async () => {
+//     const result = await notion.databases.list();
+//     console.log("DBs are: ", result);
+// }
 
-const listDB = async () => {
-    const result = await notion.databases.list();
-    console.log("DBs are: ", result);
+// listDB();
+
+
+// Querying the database "React Events"
+const getEvents = async () => {
+
+    const database_id =  process.env.NOTION_DATABASE_ID;
+
+    const payload = {
+        path: `databases/${database_id}/query`,
+        method: "POST"
+    }
+
+    // const { results } = await notion.databases.query({database_id: database_id});
+    const { results } = await notion.request(payload);
+
+    console.log("Results: ", results)
 }
-
-listDB();
+getEvents();
