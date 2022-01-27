67d530af3ae83fe5261ded4dc32c54b618900e95
Chaitanya <chaitu.vsh@gmail.com>
2022-01-28 01:47:44 +0530

    [PRO] Make requests to server, forward query params

--- APIProxyServer/routes/index.js
+++ APIProxyServer/routes/index.js
@@ -1,9 +1,43 @@
-// Imports
 /**
  * We can use Router to abstract the data providers to the controllers.
  */
+
+// Imports
 const express = require("express");
+const url = require("url");
+
+// Inbuilt routing support from express
 const router = express.Router();
+
+// Client to make requests to server
+const needle = require("needle");
+
+// ENV Variables
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
+router.get("/", async (request, response) => {
+  try {
+    //   Create params
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: [API_KEY_VALUE],
+      ...url.parse(request.url, true).query,
+    });
+
+    // Make a request to server using needle
+    const apiRes = await needle("get", `${API_BASE_URL}?${params}`);
+    const data = apiRes.body;
+
+    if (process.env.NOE_ENV !== "production") {
+      console.log(`REQUEST ${API_BASE_URL}?${params}`);
+    }
+
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+});
 
 /**
  * Route for /user end-point
